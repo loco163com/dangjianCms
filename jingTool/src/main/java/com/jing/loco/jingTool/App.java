1a46d75e89ad46c3b48c16879a486452cd565820
loco163com <loco@163.com>
2018-12-26 20:07:23 +0800

    test

---
+++ jingTool/src/main/java/com/jing/loco/jingTool/App.java
@@ -0,0 +1,10 @@
+package com.jing.loco.jingTool;
+/**
+ * Hello world!
+ *
+ */
+public class App {
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+	}
+}
---
+++ jingTool/src/test/java/com/jing/loco/jingTool/TestMine.java
@@ -0,0 +1,14 @@
+package com.jing.loco.jingTool;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TestMine {
+
+
+	@Test
+	public void testApp(){
+		assertEquals(new Long(1), new Long(1));
+	}
+}
